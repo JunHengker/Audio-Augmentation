2edd1acc3ab84ec61fa56996715d38f5032b6dac
JunHengker <juntesemail@gmail.com>
2024-11-01 22:59:03 +0700

    code update!

--- Project/main.py
+++ Project/main.py
@@ -1,123 +1,109 @@
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
-import numpy as np
-import soundfile as sf
 import os
+import soundfile as sf
+import numpy as np
+from audiomentations import (PitchShift, Shift, AddBackgroundNoise, AddGaussianSNR)
 
-
-# Total 41 augmentations available in audiomentations library
-from audiomentations import (PitchShift, Shift, TimeMask)
-
-# Function to load audio
 def load_audio(input_file):
     return librosa.load(input_file, sr=None)
 
-# Function to save audio
 def save_audio(output_file, y, sr):
     sf.write(output_file, y, sr)
 
-# Function to plot waveform and mel spectrogram
-def plot_waveform_and_mel_spectrogram(audio, sr, title=""):
-    plt.figure(figsize=(8, 4))
-    # Plot waveform
-    plt.subplot(2, 1, 1)
-    librosa.display.waveshow(audio, sr=sr)
-    plt.title(f"Waveform: {title}")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Amplitude")
-
-    # Plot mel spectrogram
-    plt.subplot(2, 1, 2)
+def save_mel_spectrogram(audio, sr, output_image_path):
+    plt.figure(figsize=(6, 4))
     S = librosa.feature.melspectrogram(y=audio, sr=sr, n_mels=128, fmax=8000)
     S_dB = librosa.power_to_db(S, ref=np.max)
     librosa.display.specshow(S_dB, sr=sr, fmax=8000)
     plt.tight_layout()
+    plt.savefig(output_image_path)
+    plt.close()
 
-# * Individual augmentation functions, could use chain transform as well
-# * P stands for probability of applying the augmentation, 1 means always apply
-
-# 1. Time Shift
 def time_shift(input_file, output_file):
     y, sr = load_audio(input_file)
-    plot_waveform_and_mel_spectrogram(y, sr, title="Original Audio")
-    
-    # Tune here
     augment = Shift(min_shift=-0.1, max_shift=0.1, p=1.0)
     y_augmented = augment(samples=y, sample_rate=sr)
-    
     save_audio(output_file, y_augmented, sr)
-    plot_waveform_and_mel_spectrogram(y_augmented, sr, title="Time Shift")
-    
-    # plt.show()
+    return y, y_augmented, sr
 
-# 2. Pitch Shift
 def pitch_shift(input_file, output_file):
     y, sr = load_audio(input_file)
-    plot_waveform_and_mel_spectrogram(y, sr, title="Original Audio")
-    
-    # Tune here
-    augment = PitchShift(min_semitones=-1, max_semitones=1, p=1.0)
+    augment = PitchShift(min_semitones=-1.5, max_semitones=-1.5, p=1.0)  # ubah jadi -1.5
     y_augmented = augment(samples=y, sample_rate=sr)
-    
     save_audio(output_file, y_augmented, sr)
-    plot_waveform_and_mel_spectrogram(y_augmented, sr, title="Pitch Shift")
-    
-    # plt.show()
+    return y, y_augmented, sr
 
-# 3. Time Masking
-def time_mask(input_file, output_file):
+def add_background_noise(input_file, output_file):
     y, sr = load_audio(input_file)
-    plot_waveform_and_mel_spectrogram(y, sr, title="Original Audio")
-    
-    # Tune here
-    augment = TimeMask(min_band_part=0.05, max_band_part=0.1, fade=True, p=1.0)
+    augment = AddBackgroundNoise(sounds_path="Project/assets/background_noises", min_snr_in_db=0, max_snr_in_db=10, p=1.0)
     y_augmented = augment(samples=y, sample_rate=sr)
-    
     save_audio(output_file, y_augmented, sr)
-    plot_waveform_and_mel_spectrogram(y_augmented, sr, title="Time Masking")
-    
-    # plt.show()
+    return y, y_augmented, sr
 
-# Mapping method names to function calls
+def add_gaussian_snr(input_file, output_file):
+    y, sr = load_audio(input_file)
+    augment = AddGaussianSNR(min_snr_in_db=25, max_snr_in_db=30, p=1.0)
+    y_augmented = augment(samples=y, sample_rate=sr)
+    save_audio(output_file, y_augmented, sr)
+    return y, y_augmented, sr
+
+
 augmentations = {
     "time_shift": time_shift,
     "pitch_shift": pitch_shift,
-    "time_mask": time_mask
+    "add_background_noise": add_background_noise,
+    "add_gaussian_snr": add_gaussian_snr
 }
 
-# Function to apply augmentation (Testing on a single file)
-def apply_augmentation(input_file, method):
-    directory = os.path.dirname(input_file)
-    base_name = os.path.basename(input_file).split('.')[0]
-    output_file = f"Project/assets/Train_Datasets/{base_name}_{method}.wav"
-    
-    # Dynamically call the augmentation method
+def process_files(directory, method):
     augment_func = augmentations.get(method)
     if augment_func:
-        augment_func(input_file, output_file)
-        print(f"Applied {method} and saved to {output_file}")
+        for filename in os.listdir(directory):
+            input_file = os.path.join(directory, filename)
+                
+            # Output files
+            base_name = os.path.splitext(filename)[0]
+            output_file = f"Project/assets/Train_Datasets/Noise_Injection/{base_name}_{method}.wav" 
+            # the augmented audio ubah (output audio)
+            output_image_after = f"Project/assets/Train_Spectograms/Noise_Injection/{base_name}_{method}.png" 
+            # augmented specto ubah (output image)
+               
+            #process the audio
+            y, y_augmented, sr = augment_func(input_file, output_file)
+            save_mel_spectrogram(y_augmented, sr, output_image_after)
+                
+            print(f"Processed: {filename}, Saved as: {output_file}")
     else:
         print(f"Method {method} not found!")
 
-# Testing on a single file, disable when applying to multiple files
-# input_file = "Project/assets/etc/335/335_AUDIO-1.wav"
-# method = "time_shift"
-# apply_augmentation(input_file, method)
-
-# Mass apply augmentations
 def process_files_with_prefix(directory, prefix, method):
-    for filename in os.listdir(directory):
-        if filename.startswith(prefix):
-            input_file = os.path.join(directory, filename)
-            apply_augmentation(input_file, method) 
-            # to change the output directory, change the output_file in apply_augmentation
+    augment_func = augmentations.get(method)
+    if augment_func:
+        for filename in os.listdir(directory):
+            if filename.startswith(prefix):
+                input_file = os.path.join(directory, filename)
+                
+                # Output files
+                base_name = os.path.splitext(filename)[0]
+                output_file = f"Project/assets/Train_Datasets/Time_Shift/{base_name}_{method}.wav" 
+                # the augmented audio ubah (output audio)
+                output_image_after = f"Project/assets/Train_Spectograms/Time_Shift/{base_name}_{method}.png" 
+                # augmented specto ubah (output image)
+               
+                #process the audio
+                y, y_augmented, sr = augment_func(input_file, output_file)
+                save_mel_spectrogram(y_augmented, sr, output_image_after)
+                
+                print(f"Processed: {filename}, Saved as: {output_file}")
+    else:
+        print(f"Method {method} not found!")
 
+# Directory and parameters
+directory = "Project/assets/Before_Test_54_Sample/noise_injection" # ubah (direktori input)
+prefix = "" # ubah (prefix angka, jadi main per batch)
+method = "add-gaussian_snr" # ubah (metodeny sesuai yang ud di define diatas)
 
-directory = "Project/assets/Before_Train_270_Sample"
-prefix = "414"
-method = "time_shift"
-
-# Remember do disable the plot function, or ur laptop will be
-# BLOWWNNN jun
-process_files_with_prefix(directory, prefix, method)
+# process_files_with_prefix(directory, prefix, method)
+# process_files(directory, method)
